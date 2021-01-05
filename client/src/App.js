9da1293326fe98fd85aebd388525cc85dbdc1f82
Alex Lar <shimanuchi@gmail.com>
2021-01-05 11:57:34 +0200

    setted up the project

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Button from '@material-ui/core/Button'
+
+function App() {
+  return (
+    <div>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+    </div>
+  );
+}
+
+export default App;
